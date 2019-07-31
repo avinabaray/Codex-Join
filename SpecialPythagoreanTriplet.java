0e48659da158dc49b9a2ae9b74a7d8f3bec47aa7
avinabaray <44506785+avinabaray@users.noreply.github.com>
2019-07-31 13:34:40 +0530

    Update SpecialPythagoreanTriplet.java

--- SpecialPythagoreanTriplet.java
+++ SpecialPythagoreanTriplet.java
@@ -1,27 +1,15 @@
 public class SpecialPythagoreanTriplet {
     public static void main(String args[])
     {
-        int a=0,b=0,c=0, flag=0;
+        int s = 1000, c;
 
-
-        for(a=1;a<=1000;a++)
-        {
-            for(b=1;b<=1000;b++)
-            {
-                for(c=1;c<=1000;c++)
-                {
-                    if(a*a+b*b==c*c && a+b+c==1000)
-                    {
-                        flag=1;
-                        break;
-                    }
+        for(int a=3; a<=(s-3)/3; a++){
+            for(int b=a+1; b<(s-a)/2; b++){
+                c=s-a-b;
+                if (c*c == a*a + b*b){
+                    System.out.print(a*b*c);
                 }
-                if(flag==1)
-                    break;
             }
-            if(flag==1)
-                break;
         }
-        System.out.println(a*b*c);
     }
 }
